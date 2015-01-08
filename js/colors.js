f26fac82e23ac4a8f19f431f59209168a9014b45
Konrad Borowski <x.fix@o2.pl>
2015-01-08 12:26:03 -0500

    Move colors around.

---
+++ js/colors.js
@@ -0,0 +1,12 @@
+var Config = {
+	customcolors: {
+		kaelcee: 'juvvia',
+		megacharizard: {color: '#CC0C0C'},
+		azum4roll: {color: '#0bb'},
+		lasszeowx: {color: '#0bb'},
+		tpplae4lance: {color: '#f55'},
+		darkfiregamer: {color: 'black'},
+		poomph: {color: '#A83838'},
+		trainerred:{color:'red'}
+	}
+};
