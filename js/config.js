3cf27aaf8dad1dd5ca6e0c7f908a7c995d9587af
Tustin2121 <tustin2121@gmail.com>
2016-07-20 16:23:56 -0400

    Added support for custom colors.

---
+++ js/config.js
@@ -0,0 +1,10 @@
+var Config = Config || {};
+
+// Custom nickname colors in chat.
+// Can be { color: "css color value" } 
+// or a nickname that hashes to the same color, by simply putting the nick
+Config.customcolors = {
+    'tustin2121' : 'tustin2121',
+    'megacharizard' : { color : "#FF0000" },
+    'abyll' : { color : "#007FFF" },
+};
