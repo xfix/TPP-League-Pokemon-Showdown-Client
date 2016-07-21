092be2a9adb1768ae305e0324dcd9442d9bfeb91
azum4roll <azum4roll@users.noreply.github.com>
2016-07-21 12:42:21 +0800

    more custom colours

--- js/config.js
+++ js/config.js
@@ -7,9 +7,10 @@ Config.customcolors = {
     'tustin2121' : 'tustin2121',
     'megacharizard' : { color : "#FF0000" },
     'abyll' : { color : "#007FFF" },
-    'azum4roll' : { color : "#00CCFF" },
+    'azum4roll' : { color : "#0088CC" },
     'darkfiregamer' : { color : "#000000" },
     'lasszeowx' : { color : "#FF7F0F" },
     'poksonkirmar' : { color : "#0000B7" },
-    'cerebralharlot' : { color : "#660066" },
+    'cerebralharlot' : { color : "#FD00FF" },
+    'coryn216' : { color : "#006778" },
 };
